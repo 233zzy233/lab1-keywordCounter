0393f73fac1ea6d29c69134a833865046b05158a
233zzy233 <90546380+233zzy233@users.noreply.github.com>
2021-09-22 20:47:11 +0800

    Add files via upload

---
+++ testcasePlus3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define UINT unsigned int
+int main(){
+    printf("int int int\
+            int\
+	         ");
+    int a =0;//int int int
+    /*int int
+    int 
+    nt*/int b=0;
+	 
+}
